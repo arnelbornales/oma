2d3bf3e448e65bf5f3aabac346c377e28a4d516b
Arnel Bornales <arnelbornales@gmail.com>
2011-12-07 16:40:25 +0800

    fix blog category page

--- wp-content/themes/oma/category.php
+++ wp-content/themes/oma/category.php
@@ -7,57 +7,60 @@
  * @since Twenty Eleven 1.0
  */
 
+
 get_header(); ?>
+<div id="main" role="main" class="wrapper-primary clear">
 
-		<section id="primary">
-			<div id="content" role="main">
+		<div id="content-blurb-wrapper" class="clear">
+			<div id="content-blurb-inner">
+			<?php //the_post(); ?>
+			<?php get_template_part( 'content', 'blurb-blog' ); ?>
+			</div>
+		</div>
+		
+		
+		<div id="content-page-wrapper" class="clear">
+    	<div id="blog-wrapper">
+      	<div id="blog-container">
+					<div id="primary">
+						<div id="content" role="main">
+								<?php if ( have_posts() ) : ?>
+												<header class="page-header">
+													<h1 class="page-title"><?php
+														printf( __( 'Category Archives: %s', 'twentyeleven' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+													?>
+													</h1>
+											  </header>
+												<?php /* Start the Loop */ ?>
+												<?php while ( have_posts() ) : the_post(); ?>
+														<?php get_template_part( 'content', 'blog' ); ?>
+												<?php endwhile; ?>
+												<?php blog_pagination(); ?>
+												
+								<?php else : ?>
+								
+												<article id="post-0" class="post no-results not-found">
+													<header class="entry-header">
+														<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+													</header><!-- .entry-header -->
+								
+													<div class="entry-content">
+														<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+														<?php get_search_form(); ?>
+													</div><!-- .entry-content -->
+												</article><!-- #post-0 -->
+								
+								<?php endif; ?>
+					  </div><!-- #content -->
+				  </div><!-- #primary -->
+				 <?php get_sidebar(); ?>
+			   </div>
+		   </div>
+		</div>		
+		
+		<div id="content-closure" class="clear">
+			<div id="content-closure-wrapper">
+			</div>
+		</div>
 
-			<?php if ( have_posts() ) : ?>
-
-				<header class="page-header">
-					<h1 class="page-title"><?php
-						printf( __( 'Category Archives: %s', 'twentyeleven' ), '<span>' . single_cat_title( '', false ) . '</span>' );
-					?>
-					</h1>
-					<?php
-						$category_description = category_description();
-						if ( ! empty( $category_description ) )
-							echo apply_filters( 'category_archive_meta', '<div class="category-archive-meta">' . $category_description . '</div>' );
-					?>
-				</header>
-
-				<?php twentyeleven_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
-				<?php endwhile; ?>
-
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
-			</div><!-- #content -->
-		</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
